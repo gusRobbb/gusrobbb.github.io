dc15639684d51ba36cf39817ae2c27f84c9b0cba
gusRobbb <gcr0379873@lpssonline.com>
2024-10-03 13:37:13 +0000

    platformer

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -53,19 +53,19 @@ createPlatform(600,200,400,10)
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-acreateCollectable("diamond",1000,400,2,true)
-acreateCollectable("database",600,0,1,true)
-acreateCollectable("max",300,710,0.1,true)
-    
+createCollectable("diamond",1000,400,2,true)
+createCollectable("database",600,0,1,true)
+createCollectable("max",300,710,0.1,true)
+createCollectable("grace",500,500,500,true)
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-acreateCannon("top",200,50,60,20)
-
-    
+createCannon("left",100,2000,80,80)
+createCannon("right",700,780,20,20)
+createCannon("right",200,800,20,20)
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
